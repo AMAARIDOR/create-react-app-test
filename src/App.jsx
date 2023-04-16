68123590f6f227cdef3ec911c32c8249b9976483
Amaari <arish2k3@gmail.com>
2023-04-16 15:53:56 +0530

    add prop for characters

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,35 @@ import Table from "./Table";
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: "Dee",
+        job: "Aspiring actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      },
+      {
+        name: "Amaari",
+        job: "Developer",
+      },
+    ];
+
     return (
-      <div className="Container">
-        <Table />
-      </div>
+      <React.StrictMode>
+        <div className="Container">
+          <Table characterData={characters} />
+        </div>
+      </React.StrictMode>
     );
   }
 }
